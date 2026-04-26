d9a9d7545790d0bdac25b4cb7a826ad12104e8b6
Dxrk System <dxrk@local>
2026-04-25 21:14:44 -0500

    Fix install.sh - pass install arg

--- install.sh
+++ install.sh
@@ -1,3 +1,2 @@
-#!/usr/bin/env bash
-set -e
-curl -fsSL https://raw.githubusercontent.com/Dxrk777/Dxrk/main/dxrk_install.py | python3
+#!/bin/bash
+curl -fsSL https://raw.githubusercontent.com/Dxrk777/Dxrk/main/dxrk_install.py -o /tmp/dxrk_install.py && python3 /tmp/dxrk_install.py install && rm /tmp/dxrk_install.py
